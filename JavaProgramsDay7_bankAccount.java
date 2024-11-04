d2c9817cf32ef2536228a5bbf08ed546c9f6db06
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-11-05 00:59:52 +0530

    Update JavaProgramsDay7_bankAccount.java

--- JavaProgramsDay7_bankAccount.java
+++ JavaProgramsDay7_bankAccount.java
@@ -1,1 +1,22 @@
+public class BankAccount {
+    private double balance;
+    private String accountHolder;
+    public BankAccount(double balance, String accountHolder) {
+        this.balance = balance;
+        this.accountHolder = accountHolder;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    public void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+
+}
 
